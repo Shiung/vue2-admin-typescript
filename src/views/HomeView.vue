41c11fe86d9e35fcc2db251ea7ddbca493508ba4
ptp_jackhsiung <ptp_jackhsiung@photons.com.tw>
2024-01-30 16:30:11 +0800

    remove useless component

--- src/views/HomeView.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <main class="text-primary p-2">
-    <Transition name="page" mode="out-in" appear>
-      <keep-alive>
-        <router-view />
-      </keep-alive>
-    </Transition>
-  </main>
-</template>

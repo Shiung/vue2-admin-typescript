bd2bb22090843ea35c2ace529a80e6aea9a6dbbf
ptp_jackhsiung <ptp_jackhsiung@photons.com.tw>
2024-01-31 13:54:20 +0800

    add i18n methods for use values object

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -5,12 +5,12 @@ import enUS from './enUS'
 
 declare module 'vue/types/vue' {
   interface Vue {
-    $i18n: (key: string) => string
+    $i18n: (key: string, replace?: { [key in string]: string }) => string
     $i18n_set: (lang: string) => void
   }
 }
 
-const flattenMethod = (o: object, prefix?: string) => {
+const flattenMethod = (o: object) => {
   return flattenObj(o)
 }
 
@@ -18,8 +18,16 @@ let i18nMap = flattenMethod(zhCN) as { [key in string]: string }
 
 export default {
   install(Vue: VueConstructor) {
-    Vue.prototype.$i18n = (key: string) => {
+    Vue.prototype.$i18n = (key: string, replace?: { [key in string]: string }) => {
       if (key in i18nMap) {
+        if (replace && typeof replace === 'object') {
+          let returnStr = i18nMap[key]
+          Object.entries(replace).forEach(([key, value]) => {
+            const reqExp = new RegExp(`{${key}}`, 'g')
+            returnStr = returnStr.replace(reqExp, value)
+          })
+          return returnStr
+        }
         return i18nMap[key]
       } else {
         return key
